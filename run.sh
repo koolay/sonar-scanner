ab6ef5d73b4c816042107fe148d8d918ad6319a2
huwl <koolay@163.com>
2017-05-23 13:45:55 +0800

    add env

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-sonar-scanner -Dsonar.projectKey=${SONAR_KEY} -Dproject.settings=${SONAR_SETTINGS}
+sonar-scanner -Dsonar.projectKey=${SONAR_KEY} -Dproject.settings=${SONAR_SETTINGS} -Dsonar.host.url=${SONAR_HOST} -Dsonar.sources=${SONAR_SOURCES}
 
